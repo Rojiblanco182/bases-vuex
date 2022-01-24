3b57fa2e1aa2e2176340db480ee5a794bd31bf1e
Rojiblanco182 <salinas.182@hotmail.com>
2022-01-24 12:30:42 +0100

    added Vuex, state and mutations

---
+++ src/components/Counter.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>Counter - Vuex</h1>
+  <h2>Direct Access: {{ $store.state.count }}</h2>
+  <h2>Computed: {{ countComputed }}</h2>
+
+    <button @click="increment">+1</button>
+    <button @click="incrementBy(5)">+5</button>
+    <button>Random</button>
+
+  <h2>MapState: {{ count }} </h2>
+  <h2>lastMutation: {{ lastMutation }} </h2>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    computed: {
+        countComputed(){
+            return this.$store.state.count
+        },
+        ...mapState(['count', 'lastMutation'])
+/*         ...mapState({
+            count: state => state.count
+        })
+ */    },
+    methods: {
+        increment() {
+            this.$store.commit('increment')
+        },
+        incrementBy(value) {
+            this.$store.commit('incrementBy', value)
+        }
+    }
+}
+</script>
