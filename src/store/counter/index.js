012ad081222530fa296a991b1ea2ac568bbfdf41
Rojiblanco182 <salinas.182@hotmail.com>
2022-01-24 18:09:11 +0100

    split counter store into single files for actions, state, getters and mutations

--- src/store/counter/index.js
+++ src/store/counter/index.js
@@ -1,38 +1,14 @@
-import getRandomInt from "../../helpers/getRandomInt"
+import state from "./state"
+import * as mutations from './mutations'
+import * as actions from './actions'
+import * as getters from './getters'
 
 const counterStore = {
     namespaced: true,
-    state: () => ({
-        count: 1,
-        lastMutation: 'none',
-        isLoading: false
-    }),
-    mutations: {
-        increment(state) {
-            state.count++
-            state.lastMutation = 'increment'
-        },
-        incrementBy(state, value) {
-            state.count += value
-            state.lastMutation = 'incrementBy'
-        },
-        setLoading(state, value) {
-            state.isLoading = value
-        }
-    },
-    actions: {
-        async incrementRandomInt(context) {
-            context.commit('setLoading', true)
-            const randomInt = await getRandomInt()
-            context.commit('incrementBy', randomInt)
-            context.commit('setLoading', false)
-        }
-    },
-    getters: {
-        squareCount(state) {
-            return state.count * state.count
-        }
-    }
+    state,
+    mutations,
+    actions,
+    getters
 }
 
 export default counterStore
